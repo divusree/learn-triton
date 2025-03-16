418e87ebc808fafa2f6a7263249cd834e916b7b9
divusree <divusree@gmail.com>
2025-03-16 12:01:22 +0530

    second kernel to run output = p/l

--- softmax/softmax.py
+++ softmax/softmax.py
@@ -35,7 +35,39 @@ def softmax_kernel(x_ptr, m_ptr, l_ptr,output_ptr,
 
     tl.store(m_ptr + offset_xm[:,None] * stride_mm, m)
     tl.store(l_ptr + offset_xm[:,None] * stride_lm, l)
+@triton.jit
+def softmax_kernel2(x_ptr, m_ptr, l_ptr,output_ptr,
+                    M, N, 
+                    stride_xm, stride_xn, 
+                    stride_mm, stride_mn, 
+                    stride_lm, stride_ln, 
+                    stride_om, stride_on, 
+                    BLOCK_SIZE_M: tl.constexpr, 
+                    BLOCK_SIZE_N: tl.constexpr 
+                    ):
+    pid_m = tl.program_id(axis = 0)
+    pid_n = tl.program_id(axis = 1)
 
+    offset_xm = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offset_xn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    offset =  offset_xm[:,None] * stride_xm + offset_xn[None,:] *stride_xn
+    x_ptr += offset
+
+    offset_output =  offset_xm[:,None] * stride_om + offset_xn[None,:] *stride_on
+    output_ptr += offset_output 
+    m_ptr += offset_xm[:,None] * stride_mm
+    l_ptr += offset_xm[:,None] * stride_lm
+    m = tl.load(m_ptr)
+    l = tl.load(l_ptr)
+    for i in range(0, tl.cdiv(N, BLOCK_SIZE_N)):
+        x = tl.load(x_ptr , mask = (offset_xm[:,None]  < M) & (offset_xn[None, :] < N)) #, mask = (offset < N), other = 0)
+        p = tl.exp(x - m)
+        # debug the issue here. softmax value is right only for first row
+        
+        tl.store(output_ptr, tl.div_rn(p, l), mask = (offset_xm[:,None]  < M) & (offset_xn[None, :] < N))
+        
+        x_ptr += BLOCK_SIZE_N *  stride_xn
+        output_ptr += BLOCK_SIZE_N *  stride_on
 def softmax(x: torch.Tensor):
     M, N = x.shape
     m = torch.zeros((M, 1), device = 'cuda', dtype = torch.float32)
