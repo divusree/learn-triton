925264b148ebbbd0fae9725846e18af0dd4e75a4
divusree <divusree@gmail.com>
2025-03-31 12:05:58 +0530

    batched softmax BxMxN

--- softmax/softmax.py
+++ softmax/softmax.py
@@ -1,26 +1,25 @@
 import triton
 import triton.language as tl
+import triton
+import triton.language as tl
 import torch
-
-
 @triton.jit
 def softmax_kernel(
     x_ptr,
     output_ptr,
     M: tl.constexpr,
     N: tl.constexpr,
-    stride_xm,
-    stride_xn,
-    stride_om,
-    stride_on,
+    stride_xb, stride_xm, stride_xn,
+    stride_ob, stride_om, stride_on,
     BLOCK_SIZE_M: tl.constexpr,
     BLOCK_SIZE_N: tl.constexpr,
 ):
-    pid_m = tl.program_id(axis=0)
-    pid_n = tl.program_id(axis=1)
+    pid_b = tl.program_id(axis=0) 
+    pid_m = tl.program_id(axis=1)
+    pid_n = tl.program_id(axis=2)
     offset_xm = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
     offset_xn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % BLOCK_SIZE_N
-    offset = offset_xm[:, None] * stride_xm + offset_xn[None, :] * stride_xn
+    offset =  pid_b * stride_xb + offset_xm[:, None] * stride_xm + offset_xn[None, :] * stride_xn
     x_ptr += offset
     mask = (offset_xm[:, None] < M) & (offset_xn[None, :] < N)
     # notation is from flash attrntion 2 paper
@@ -29,29 +28,21 @@ def softmax_kernel(
     p = tl.exp(s - m)
     l = tl.sum(p, axis=1, keep_dims=True)  # correction factor = 0
 
-    output_offset = (
+    output_offset = ( pid_b * stride_ob + 
         offset_xm[:, None] * stride_om
         + (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N))[None, :] * stride_on
     )
     tl.store(output_ptr + output_offset, p / l, mask=mask)
 
-
 def softmax(x: torch.Tensor):
-    M, N = x.shape
-    output = torch.zeros((M, N), device="cuda", dtype=torch.float32)
+    B, M, N = x.shape
+    output = torch.zeros((B, M, N), device = 'cuda', dtype = torch.float32)
     assert x.is_cuda
-    grid = lambda meta: (triton.cdiv(M, meta["BLOCK_SIZE_M"]), 1)
-    softmax_kernel[grid](
-        x,
-        output,
-        M,
-        N,
-        x.stride(0),
-        x.stride(1),
-        output.stride(0),
-        output.stride(1),
-        BLOCK_SIZE_M=32,
-        BLOCK_SIZE_N=triton.next_power_of_2(N),
-    )
-
+    grid = lambda meta: (B, triton.cdiv(M, meta['BLOCK_SIZE_M']))
+    softmax_kernel[grid](x, output,
+                         M, N,      
+                    *x.stride(), 
+                    *output.stride(), 
+                    BLOCK_SIZE_M = 32,
+                    BLOCK_SIZE_N = triton.next_power_of_2(N))   
     return output
