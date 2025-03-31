c4f38301b2c906957cbe76ccdb7ac99278d1ea57
divusree <divusree@gmail.com>
2025-03-31 18:12:55 +0530

    checkpoint for BxHxMxN dims

--- multiplication/multiplication.py
+++ multiplication/multiplication.py
@@ -141,8 +141,7 @@ def group_matmul_kernel(
     offset_on = pid_n + tl.arange(0, BLOCK_SIZE_N)
     output_offset = offset_om[:,None] *stride_om + offset_on[None, :] *stride_on
     tl.store(output_ptr + output_offset, accumulator, mask = (offset_om[:,None] < M) &( offset_on[None, :] < N))
-
-def matmul(x: torch.Tensor, y: torch.Tensor, dtype = None):
+def matmul3D(x: torch.Tensor, y: torch.Tensor, dtype = None):
     M , K = x.shape
     K1, N = y.shape
     assert K == K1
@@ -163,6 +162,84 @@ def matmul(x: torch.Tensor, y: torch.Tensor, dtype = None):
                         x.stride(0), x.stride(1),
                         y.stride(0), y.stride(1),
                         output.stride(0), output.stride(1),
+                        )
+    return output
+
+@triton.jit
+def batch_matmul_kernel(
+                # my pointers
+                x_ptr,y_ptr,output_ptr,
+                # matrix dims
+                B, H, M, K, N,
+                # strides for each matrix
+                stride_xb, stride_xh, stride_xm, stride_xk,
+                stride_yb, stride_yh, stride_yk, stride_yn,
+                stride_ob, stride_oh, stride_om, stride_on,
+                BLOCK_SIZE_M: tl.constexpr, 
+                BLOCK_SIZE_N: tl.constexpr, 
+                BLOCK_SIZE_K: tl.constexpr,
+                GROUP_SIZE_M: tl.constexpr):
+
+    pid_batch = tl.program_id(axis=0) 
+    pid_m = tl.program_id(axis=1)
+    pid_n = tl.program_id(axis=2)
+
+    pid_b = pid_batch // H
+    pid_h = pid_batch % H
+
+    # Number of program ids along M dimension
+    # num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    # num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+
+    # pid_m = pid_mat // N
+    # pid_n = pid_mat % N
+
+    # first_pid_m = pid_m * GROUP_SIZE_M
+    # group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+    # pid_m = first_pid_m + (pid_mat % group_size_m)
+
+    # strides for the M, K and N dim. 
+    # A[b, i, j] = b * stride_Ab  + i *stride_Am + j * stride_An
+    # &A[b, m : m+BLOCK_SIZE_M, k:k+BLOCK_SIZE_K] =  a_ptr + A.stride(0) + (m : m+BLOCK_SIZE_M)*A.stride(1) + (k : k+BLOCK_SIZE_K)*A.stride(2);
+    offset_xm = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M))
+    offset_k =  tl.arange(0, BLOCK_SIZE_K)
+    offset_yn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N))
+
+    x_ptr += pid_b * stride_xb + pid_h * stride_xh + offset_xm[:,None] * stride_xm +  offset_k[None,:]*stride_xk
+    y_ptr += pid_b * stride_yb + pid_h * stride_yh + offset_k[:,None] * stride_yk + offset_yn[None,:] *stride_yn
+
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype = tl.float32)
+    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+        x = tl.load(x_ptr, mask = offset_k [None, :] < K - k * BLOCK_SIZE_K, other = 0.0)
+        y = tl.load(y_ptr, mask = offset_k[:,None] < K - k * BLOCK_SIZE_K, other = 0.0)            
+        accumulator = tl.dot(x, y, accumulator)
+        
+        # advance to next k block 
+        x_ptr += BLOCK_SIZE_K *  stride_xk
+        y_ptr += BLOCK_SIZE_K *  stride_yk
+        
+    offset_om = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offset_on = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    output_offset = pid_b * stride_ob + pid_h * stride_oh + offset_om[:,None] *stride_om + offset_on[None, :] *stride_on
+    output_mask =  (offset_om[:,None] < M) & ( offset_on[None, :] < N)
+    tl.store(output_ptr + output_offset, accumulator, mask = output_mask)
+       
+def matmul(x: torch.Tensor, y: torch.Tensor):
+    B, H, M , K = x.shape
+    B1, H1, K1, N = y.shape
+    assert (K == K1) & (B == B1) * (H == H1)
+    output = torch.empty((B, H, M, N), device = 'cuda', dtype = y.dtype)
+    assert x.is_cuda and y.is_cuda and output.is_cuda
+
+    grid = lambda meta: (B*H, triton.cdiv(M, meta['BLOCK_SIZE_M']), triton.cdiv(N, meta['BLOCK_SIZE_N']))
+    batch_matmul_kernel[grid](x, y, output, 
+                        B, H, M, K, N, 
+                        *x.stride(), *y.stride(), *output.stride(),
+                        BLOCK_SIZE_M = 32, 
+                        BLOCK_SIZE_N = 32, 
+                        BLOCK_SIZE_K = 32, 
+                        # GROUP_SIZE along M
+                        GROUP_SIZE_M = 8
                         )
     return output
 
